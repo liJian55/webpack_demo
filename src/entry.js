bbb76977f94df0539d02a461f6170ce066d6e1fa
Rs-jim <373008676@qq.com>
2017-12-22 16:31:24 +0800

    更新优化黑技能

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,10 @@
 import css from './css/index.css';
 import less from'./css/black.less';
-document.getElementById('title').innerHTML="hello webpack";
-alert("给拜个早年了");
+import sass from'./css/bb.scss';
+import { join } from 'path';
+
+{
+const jim = 'webpack 祝你冬至快乐'
+document.getElementById('title').innerHTML=jim;
+}
+$('#saq').html('你好李坚');
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
 const path = require('path');
-const uglify = require('uglifyjs-webpack-plugin');
-const htmlPlugin = require('html-webpack-plugin');
-const extractTextPlugin = require("extract-text-webpack-plugin");
-var website = {
-    publicpath:"http://192.168.2.172:8081/"
+const entry = require('./webpack_config/enrty_webpack')
+const extractTextPlugin = require('extract-text-webpack-plugin');//压缩css
+const PrifyCssPlugin = require('purifycss-webpack');
+const uglify = require('uglifyjs-webpack-plugin');//压缩js
+const htmlPlugin = require('html-webpack-plugin');//插件-- 加哈希 清除attribute
+const glob = require('glob');
+const webpack = require('webpack');
+
+
+
+console.log(encodeURIComponent(process.env.type))
+if(process.env.type=="build"){
+    var website = {
+        publicpath:"http://jspang.com:1717/"
+    }
+   
+}else{
+    var website = {
+        publicpath:"http://192.168.2.172:8081/"
+    }
 }
 module.exports={
+    //devtool:'source-map',
     entry:{
-        entry:'./src/entry.js'
+        entry:'./src/entry.js',
+        jquery:'jquery',
+        vue:'vue'
     },
     output:{
         path:path.resolve(__dirname,'dist'),
@@ -20,7 +38,10 @@ module.exports={
                 test:/\.css$/,
                 use:extractTextPlugin.extract({
                     fallback:"style-loader",
-                    use:"css-loader"
+                    use:[
+                        {loader:"css-loader",options:{improtLoaders:1}},
+                        'postcss-loader'
+                    ]
                 })
             },{
                 test:/\.(png|jpg|jpeg|gif)/,
@@ -44,11 +65,37 @@ module.exports={
                     }],
                     fallback:'style-loader'
                 })
+            },{
+                test:/\.scss/,
+                use:extractTextPlugin.extract({
+                    use:[
+                        {loader:'css-loader'},
+                        {loader:'sass-loader'},
+                    ],
+                    fallback:'style-loader'
+                    
+                })
+            },{
+                test:/\.(jsx|js)$/,
+                use:{
+                    loader:'babel-loader'
+                },
+                exclude:/node_modules/
             }
         ]
     },
     plugins:[
+        new webpack.optimize.CommonsChunkPlugin({
+            name:['jquery','vue'],
+            filename:'assets/js/[name].js',
+            minChunks:2
+        }),
+
+
         //new uglify()
+        new webpack.ProvidePlugin({
+            $:"jquery",
+        }),
         new htmlPlugin({
             minify:{
                 removeAttributeQuotes:true
@@ -56,13 +103,23 @@ module.exports={
             hash:true,
             template:'./src/index.html'
         }),
-        new extractTextPlugin("css/index.css")
+        new extractTextPlugin("css/index.css"),
+        new PrifyCssPlugin({
+            paths:glob.sync(path.join(__dirname,'src/*.html')) 
+        }),
+        new webpack.BannerPlugin('李坚版权所有')
     ],
     devServer:{
         contentBase:path.resolve(__dirname,'dist'),
         host:'192.168.2.172',
         compress:true,
         port:8081
-    }
+    },
+    // watchOptions:{
+    //     poll:1000,//检测修改的时间
+    //     //aggregeateTimeout:500,//防止重复按键 500ms内 webpack3.10貌似已移除
+    //     //ignored:/node_modules/, //不检测
+
+    // }
     
 }
